d8d5e9ff7bc952df503c5fdd51d160663e7e34a0
Gun4ar <quwaterman@ya.ru>
2018-03-17 09:48:27 +0300

    Test Y Last Row

--- src/Game.java
+++ src/Game.java
@@ -52,7 +52,9 @@ public class Game {
             /**Y middle row*/
         } else if (cells[1][0] == cells[1][1] && cells[1][1] == cells[1][2] && cells[1][1] != '.') {
             return cells[1][0] == 'X' ? Status.X_WIN : Status.O_WIN;
-        }
+        }     /**Y last row*/
+         else if (cells[2][0] == cells[2][1] && cells[2][1] == cells[2][2] && cells[2][1] != '.') {
+        return cells[2][0] == 'X' ? Status.X_WIN : Status.O_WIN;}
 
 
         for (int i = 0; i < SIZE; i++) {
