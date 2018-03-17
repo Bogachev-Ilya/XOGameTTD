4d9a210898854534311505ef718c5f591635561f
Gun4ar <quwaterman@ya.ru>
2018-03-17 09:50:31 +0300

    Test Diagonal Left Right

--- src/Game.java
+++ src/Game.java
@@ -48,13 +48,17 @@ public class Game {
             return cells[0][1] == 'X' ? Status.X_WIN : Status.O_WIN;
             /**X last row*/
         } else if (cells[0][2] == cells[1][2] && cells[1][2] == cells[2][2] && cells[1][2] != '.') {
-        return cells[0][2] == 'X' ? Status.X_WIN : Status.O_WIN;
+            return cells[0][2] == 'X' ? Status.X_WIN : Status.O_WIN;
             /**Y middle row*/
         } else if (cells[1][0] == cells[1][1] && cells[1][1] == cells[1][2] && cells[1][1] != '.') {
             return cells[1][0] == 'X' ? Status.X_WIN : Status.O_WIN;
         }     /**Y last row*/
-         else if (cells[2][0] == cells[2][1] && cells[2][1] == cells[2][2] && cells[2][1] != '.') {
-        return cells[2][0] == 'X' ? Status.X_WIN : Status.O_WIN;}
+        else if (cells[2][0] == cells[2][1] && cells[2][1] == cells[2][2] && cells[2][1] != '.') {
+            return cells[2][0] == 'X' ? Status.X_WIN : Status.O_WIN;
+        }   /**Diagonal left-right*/
+        else if (cells[0][0] == cells[1][1] && cells[1][1] == cells[2][2] && cells[1][1] != '.') {
+            return cells[0][0] == 'X' ? Status.X_WIN : Status.O_WIN;
+        }
 
 
         for (int i = 0; i < SIZE; i++) {
